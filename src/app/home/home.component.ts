6bbf687e254c3141ac40012f21f4f60cf9b96b89
Akash11gavhane <akash1106gavhane@gmail.com>
2023-11-29 01:07:11 +0530

    coursel added to home ui successfull

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,17 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HomeComponent {
 
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  popularProducts:undefined | product[]
+
+  constructor(private product:ProductService){
+
+  }
+
+  ngOnInit():void{
+    this.product.popularProducts().subscribe((data)=>{
+      console.warn(data)
+      this.popularProducts=data
+    })
+  }
 
 }
